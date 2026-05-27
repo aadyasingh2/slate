96cc12444ebc1638df1c52f84f8b5360a55b44f2
aadyasingh2 <aadya2630@gmail.com>
2026-05-27 23:57:08 +0530

    basic routing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Landing from './pages/Landing'
+import StudySession from './pages/StudySession'
 function App() {
-
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='/session/:id' element={<StudySession />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
